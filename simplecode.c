923fda31e6f620bb7aa4f0e3be874bf1882d652f
AbdiAbdurahman <abdirahman.mabdurahman@gmail.com>
2026-01-21 01:20:14 +0000

    simple c program to sum an array of ints

--- simplecode.c
+++ simplecode.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = sizeof(arr) / sizeof(arr[0]);
     int sum = 0;
 
